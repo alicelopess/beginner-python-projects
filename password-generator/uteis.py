d1be4afa3b5cb52c8686790c3e45572df7c9c7f5
Alice Lopes <alicedflopes.02@gmail.com>
2023-10-26 18:11:05 -0300

    uptade the files

--- password-generator/uteis.py
+++ password-generator/uteis.py
@@ -1,22 +1,16 @@
 import random
 from constants import const_list
 
-def usage():
-  print('Usage: Not a valid number!')
-  q = str(input('Do you want to end the program [Y/N]? '))
-  if 'Y' in q or 'y' in q:
-    print('Ending program...')
-    return 'break'
-  #Senha menor do que o número de caracteres
-  
 def pwd_info():
   pwd_dict = {}
+
 
   pwd_dict['number'] = int(input('Please, enter the number of passwords: '))
   pwd_dict['lenght'] = int(input('Please, enter the lenght of the password: '))
   pwd_dict['special characters'] = int(input('Please, enter the number of special characters: '))
   pwd_dict['uppercase'] = int(input('Please, enter the number of uppercase characters: '))
   pwd_dict['numbers'] = int(input('Please, enter the number of integers: '))
+
 
   return pwd_dict
 
@@ -25,23 +19,42 @@ def generate_pwd(pwd_dict):
 
   for i in range(pwd_dict['number']):
     counter = pwd_dict['lenght']
-    pwd = ''
+    pwd = []
     for i in range(pwd_dict['special characters']):
-      pwd += random.choice(const_list[2])
+      pwd.append(random.choice(const_list[2]))
       counter -= 1
     for i in range(pwd_dict['uppercase']):
-      pwd += random.choice(const_list[1])
+      pwd.append(random.choice(const_list[1]))
       counter -= 1
     for i in range(pwd_dict['numbers']):
-      pwd += random.choice(const_list[3])
+      pwd.append(random.choice(const_list[3]))
       counter -= 1
     for i in range(counter):
-      pwd += random.choice(const_list[0])
+      pwd.append(random.choice(const_list[0]))
       counter -= 1
+    random.shuffle(pwd)
+    pwd = ''.join(pwd)
     pwds.append(pwd)
-  
-  print(pwds)
-  for item in pwds:
-    print(random.shuffle(pwds))
-  
+
   return pwds
+
+def usage(item, subitem=None):
+  if item == 0:
+  #Tamanho de senha invalido
+    print('Usage: Not a valid number!')
+    while True:
+      q = str(input('Do you want to end the program [Y/N]? '))
+      if 'Y' in q or 'y' in q:
+        return 'break'
+  else:
+    pass
+  
+  if subitem > item:
+  #Senha menor do que o número de caracteres
+    print('Usage: Not a valid lenght!')
+    q = str(input('Do you want to end the program [Y/N]? '))
+    if 'Y' in q or 'y' in q:
+      print('Ending program...')
+      return 'break'
+  else:
+    pass
